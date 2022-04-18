ae50395752715589dc83b58f35c30e323f4b464b
littleet9 <littleet@appstate.edu>
2022-04-18 18:01:49 -0400

    Added Mob Registry Handler

--- src/main/java/com/example/examplemod/RegistryHandler.java
+++ src/main/java/com/example/examplemod/RegistryHandler.java
@@ -1,5 +1,6 @@
 package com.example.examplemod;
 
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
@@ -16,17 +17,14 @@ public class RegistryHandler {
     // create DeferredRegister objects
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ExampleMod.MODID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ExampleMod.MODID);
-    //public static final DeferredRegister<EntityType> MOBS = DeferredRegister.create(ForgeRegistries.ENTITIES)
+    public static final DeferredRegister<EntityType<?>> MOBS = DeferredRegister.create(ForgeRegistries.ENTITIES, ExampleMod.MODID);
 
     public static void init() {
         // attach DeferredRegisters to the event bus
         BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        MOBS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
-/***************************************
-    End Phantom
- ***************************************/
-
 
 /***************************************
     End Moss Block
